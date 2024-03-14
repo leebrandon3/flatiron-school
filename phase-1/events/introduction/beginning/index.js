7c225e30dbbe3963685af993e7b1ebecf1081121
Brandon <brandonslee3@gmail.com>
2024-03-14 00:12:47 -0400

    update notes

--- phase-1/events/introduction/beginning/index.js
+++ phase-1/events/introduction/beginning/index.js
@@ -17,8 +17,26 @@ Takeaways
 */
 
 // ~ the "click" event
+// The target (what element the event will fire on, e.g. #today), the event (the user action, "click"), and the handler (log)
+//document.getElementById("today")
+document.querySelector("#today").addEventListener("click", event => {
+    console.log("The user clicked on Today!")
+})
 
 // ~ the "submit" event
+document.querySelector("form").addEventListener("submit", event => {
+    event.preventDefault()
+    console.log("The form was submitted!")
+    console.log(document.querySelector("form").date)
+    const submittedDate = document.querySelector("form").date.value
+    console.log(submittedDate)
+})
 
 // ~ challenges
 // 1. Add some non-click, non-submit event listener to the page. It can trigger whatever you wish!
+
+document.getElementById("tomorrow").addEventListener("mouseover", () => {
+    const image = document.createElement("img")
+    image.src = "https://media1.tenor.com/m/ShzdJcrguswAAAAC/burn-elmo.gif"
+    document.body.append(image)
+})
